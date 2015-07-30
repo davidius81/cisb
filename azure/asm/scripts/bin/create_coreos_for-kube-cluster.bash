9364d2a969162998595aeeae504080c692ef47d6
root <root@dlscusvmtest1.dl-scus-cs-test1.j7.internal.cloudapp.net>
2015-07-30 20:16:11 +0000

    Delete old azure folder

--- azure/asm/scripts/bin/create_coreos_for-kube-cluster.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..3}
-do
-
-azure vm create --custom-data=/opt/cgi/cloud-config/cloud-config-kube.yaml  --ssh=4100$i --ssh-cert=/opt/cgi/cert/key/myCert.pem --no-ssh-password --vm-name=coseukubenode$i --availability-set eastuscsib --connect=cosneumgmtcs01 --virtual-network-name cosneuvnet010 --subnet-names puppetvlan010,dockers 2b171e93f07c4903bcad35bda10acf22__CoreOS-Alpha-709.0.0 core
-
-done
