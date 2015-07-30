639e662bbb8bf1e3c1b1a63fd9605571a48cb905
root <root@dlscusvmtest1.dl-scus-cs-test1.j7.internal.cloudapp.net>
2015-07-30 20:25:17 +0000

    Moving script to Azure Folder

--- Azure/asm/scripts/azurecli/test/data/set-chef-extension-client-config.rb
+++
@@ -1,4 +0,0 @@
-log_level        :info
-log_location     "c:/chef/logs"
-chef_server_url  "https://api.opscode.com/organizations/orgname"
-validation_client_name 	"orgname-validator"
--- Azure/asm/scripts/azurecli/tools/windows/scripts/disableFiddler.cmd
+++
@@ -1,7 +0,0 @@
-::
-:: Microsoft Azure CLI - scripts to disable tracing through fiddler
-:: Copyright (C) Microsoft Corporation. All Rights Reserved.
-::
-
-set NODE_TLS_REJECT_UNAUTHORIZED=
-set HTTPS_PROXY=
